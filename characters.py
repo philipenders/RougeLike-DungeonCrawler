6b5c7a7a334e41a688c3076296c1a894f8a44131
Philip Enders <philip.enders1@gmail.com>
2017-12-03 21:49:49 -0500

    slight change to player and items files

--- characters.py
+++ characters.py
@@ -1,12 +1,17 @@
+import random
 class Character(object):
     def __init__(self):
         self.hp = 10
-
-
+        self.strength = 10 #Base damage
+        self.defense = 5
+        self.speed = 5 #number of tiles movable per turn
+    def attack(self, target):
+        target.hp -= max[(self.weapon.base_damage - (target.defense +target.armor.base_armor)), 1]
 
 class Player(Character):
-    def __init__(self):
+    def __init__(self,name):
         super(Player,self).__init__()
+        self.weapon = "pointy_stick"
 
 
 class Enemy(Character):
--- items.py
+++ items.py
@@ -1,13 +1,39 @@
-class Item(object):
-    def __init__(self):
-        self.value = 12
+import characters
 
+class Item(object):
+    def __init__(self,name,owner):
+        self.value = 3
+        self.owner = owner
+        self.name = name
 
 class Weapon(Item):
-    def __init__(self):
+    def __init__(self, damage):
         super(Weapon,self).__init__()
+        self.base_damage = self.owner.strength +damage
+        self.value = (3*damage) + self.value
+
+class Armor(Item):
+    def __init__(self):
+        super(Armor, self).__init__()
+
+
+class Boots(Item):
+    def __init__(self):
+        super(Boots,self).__init__()
+
+class MiscWearable(Item):
+    def __init__(self):
+        super(MiscWearable,self).__init__()
+
+
+class MiscOther(Item):
+    def __init__(self):
+        super(MiscOther,self).__init()
 
 
 
-myWeapon = Weapon()
+
+pointy_stick = Weapon(characters.Player.weapon,2)
+
+myPlayer = Player("Jask")
 
